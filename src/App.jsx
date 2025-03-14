ac2323eab71bf6fa4195d433336c390ce9c9a1ac
AmandMilette <amand.milette@gmail.com>
2025-03-14 10:22:25 -0400

    installed packages for app functionality and created route template

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Header />
+        <div className="main__container">
+          <Routes>
+            <Route path="/" element={<Navigate to="/warehouses" />} />
+            <Route path="/warehouses" element={<>ToDo:EditInventoryPage</>} />
+            <Route
+              path="/warehouses/:id"
+              element={<>ToDo:EditInventoryPage</>}
+            />
+            <Route
+              path="/warehouses/:id/edit"
+              element={<>ToDo:EditInventoryPage</>}
+            />
+            <Route
+              path="/warehouses/add"
+              element={<>ToDo:EditInventoryPage</>}
+            />
+            <Route path="/inventory" element={<>ToDo:EditInventoryPage</>} />
+            <Route
+              path="/inventory/:id"
+              element={<>ToDo:EditInventoryPage</>}
+            />
+            <Route
+              path="/inventory/:id/edit"
+              element={<>ToDo:EditInventoryPage</>}
+            />
+            <Route
+              path="/inventory/add"
+              element={<>ToDo:EditInventoryPage</>}
+            />
+            <Route path="*" element={<h1>404 NOT FOUND</h1>} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
