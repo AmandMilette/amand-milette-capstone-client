f37c3f963f7fce672956261769818bad5812c813
AmandMilette <amand.milette@gmail.com>
2025-03-23 23:45:24 -0400

    added text content for About page

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -6,7 +6,82 @@ function AboutPage() {
   return (
     <>
       <main className="about-main-content">
-        <h1>About Comfort Companions Pet Care</h1>
+        <h1>About</h1>
+        <section>
+          <h2>Cozy Care For All Dogs & Cats</h2>
+          <h3>Pet Care Experience</h3>
+          <p>
+            Hello, I'm Amand (they/them). I have grown up with dogs my entire
+            life, and I started taking more of a direct role in taking care of
+            the family dogs over the years when I was 10 years old. I have
+            experience taking care of small, medium, and large-sized dogs from
+            potty-trained puppies to senior cuties. I have provided dog walking,
+            doggy daycare, drop-in visit, and house-sitting services for many
+            family members and friends over the years.
+          </p>
+          <p>
+            I also have 1 year of experience in taking care of cats. I have an
+            equal love of cats and dogs in my heart to provide the best care for
+            every pet. I have experience with taking care of single-cat and
+            multiple-cat households. I have provided drop-in and house-sitting
+            services to many of my cat-owning friends.
+          </p>
+          <h3>Safety, Trust & Environment</h3>
+          <p>
+            I live in a pet-friendly apartment building a short distance away
+            from multiple parks & walking trails. There is a small grass area
+            beside the main entrance of my building where I can easily take dogs
+            out for potty breaks. I am part of the LGBTQ+ community and
+            COVID-conscious (I mask indoors with KN95/N95, have up-to-date
+            vaccines, and follow isolation protocols) so I will ensure that your
+            pets are taken care of in my home and yours will be treated in the
+            same way they are with you. I am comfortable with administering
+            medication for pets, collecting mail, cleaning up pet-related
+            messes, and minor grooming maintenance if requested as part of my
+            services.
+          </p>
+          <p>
+            For dog boarding, Doggy Daycare, house-sitting and drop in services
+            I am able to accept clients of multiple pet households.
+          </p>
+          <p>
+            Please let me know if your precious pet has any special needs
+            (physical, behavioral, etc.) in order for me to properly accommodate
+            them and to provide the highest quality of care with my services.
+          </p>
+        </section>
+        <section>
+          <h2>Reviews</h2>
+          <h3>Claire R.</h3>
+          <p>Jan 31, 2024</p>
+          <p>
+            Amand was kind enough to look after my cats, Baby and Charlie for
+            three months while my husband and I were on an extended vacation. We
+            were introduced to Amand through a friend, and Amand was very prompt
+            with communication as we organized the housesitting and cat sitting
+            details. Amand was reliable and attentive, sending regular updates.
+            Our shy Charlie and Baby felt very comfortable and at home the whole
+            time. We were able to enjoy our time away knowing our home and our
+            beloved kitties were well-taken care of. I would absolutely
+            recommend Amand to anyone looking to have a caring and dependable
+            caregiver for their pets!
+          </p>
+          <h3>Jessica M.</h3>
+          <p>Jan 30, 2024</p>
+          <p>
+            Amand is a passionate pet lover, having had mixed breed dogs (small
+            and medium) growing up and into adulthood. Amand in addition to
+            walking your pet can assist in paw care, bathing, feeding, cleaning,
+            and administering medications. Amand has experience walking and pet
+            sitting young pets and seniors. They have provided in home animal
+            boarding for cats and dogs while loved ones have been away on
+            vacation. Amand has an eye for detail and will ensure your pet gets
+            the enrichment, love, and care all in a way that meets your pet's
+            lifestyle and schedule. Amand is also familiar with enrichment and
+            can get your pet set up for fun using treats and a blanket to create
+            an impromptu snuffle mat.
+          </p>
+        </section>
       </main>
     </>
   );
